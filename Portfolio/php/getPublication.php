6128f6b05930f94f28839ee9fe4474a81006917b
Damien Lanusse <damienl.work@gmail.com>
2023-02-05 17:50:38 +0100

    getPublication16

--- Portfolio/php/getPublication.php
+++ Portfolio/php/getPublication.php
@@ -1,6 +1,10 @@
 <?php
     header('Content-Type: text/xml');
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     // On définit les variables nécessaires au lien avec la BD
     $bdd = "u562708442_dlanusse";
     $host = "localhost";
