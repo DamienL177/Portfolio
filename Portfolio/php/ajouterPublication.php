461cb814f1c09880108caed06184ea24777edfbf
Damien Lanusse <damienl.work@gmail.com>
2023-02-05 16:15:44 +0100

    Insertion de publication fonctionnel

--- Portfolio/php/ajouterPublication.php
+++ Portfolio/php/ajouterPublication.php
@@ -1,5 +1,5 @@
 <?php
-    //header('Location: ../ajouterPublication.html');
+    header('Location: ../ajouterPublication.html');
 
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
@@ -33,44 +33,49 @@
             // ON PLACE L'IMAGE DANS SON DOSSIER
             mkdir($chemin, 0755, true);
 
-            $fichierCible = $chemin . basename($_FILES["image"]["name"]);
-            $uploadOk = 1;
-            $imageFileType = strtolower(pathinfo($fichierCible,PATHINFO_EXTENSION));
-
-            // Check if image file is a actual image or fake image
-            $check = getimagesize($_FILES["image"]["tmp_name"]);
-            if($check !== false) {
-                //echo "File is an image - " . $check["mime"] . ".";
+            if(isset($_FILES["image"])){
+                $fichierCible = $chemin . basename($_FILES["image"]["name"]);
                 $uploadOk = 1;
-            } else {
-                echo "File is not an image.";
-                $uploadOk = 0;
-            }
+                $imageFileType = strtolower(pathinfo($fichierCible,PATHINFO_EXTENSION));
 
-            // Check file size
-            if ($_FILES["image"]["size"] > 500000) {
-                echo "Sorry, your file is too large.";
-                $uploadOk = 0;
-            } 
+                // Check if image file is a actual image or fake image
+                $check = getimagesize($_FILES["image"]["tmp_name"]);
+                if($check !== false) {
+                    //echo "File is an image - " . $check["mime"] . ".";
+                    $uploadOk = 1;
+                } else {
+                    echo "File is not an image.";
+                    $uploadOk = 0;
+                }
 
-            // Allow certain file formats
-            if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                $uploadOk = 0;
-            } 
+                // Check file size
+                if ($_FILES["image"]["size"] > 500000) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                } 
 
-            // Check if $uploadOk is set to 0 by an error
-            if ($uploadOk == 0) {
-                echo "Sorry, your file was not uploaded.";
-            // if everything is ok, try to upload file
-            } 
-            else {
-                if (move_uploaded_file($_FILES["image"]["tmp_name"], $fichierCible)) {
-                    //echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+                // Allow certain file formats
+                if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+                    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+                    $uploadOk = 0;
+                } 
+
+                // Check if $uploadOk is set to 0 by an error
+                if ($uploadOk == 0) {
+                    echo "Sorry, your file was not uploaded.";
+                // if everything is ok, try to upload file
                 } 
                 else {
-                    echo "Sorry, there was an error uploading your file.";
+                    if (move_uploaded_file($_FILES["image"]["tmp_name"], $fichierCible)) {
+                        //echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+                    } 
+                    else {
+                        echo "Sorry, there was an error uploading your file.";
+                    }
                 }
+            }
+            else{
+                $fichierCible = null;
             }
 
             // ON PLACE TOUT DANS LA BD
