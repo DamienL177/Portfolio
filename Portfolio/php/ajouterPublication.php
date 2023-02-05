43ab43d3f595119e22118f391d1aebace87e8998
Damien Lanusse <damienl.work@gmail.com>
2023-02-05 14:37:46 +0100

    Correction d'erreur 9

--- Portfolio/php/ajouterPublication.php
+++ Portfolio/php/ajouterPublication.php
@@ -33,12 +33,11 @@
             // ON PLACE L'IMAGE DANS SON DOSSIER
             mkdir($chemin, 0755, true);
 
-            $fichierCible = $chemin . basename($_POST["image"]["name"]);
+            $fichierCible = $chemin . basename($_FILES["image"]["name"]);
             $uploadOk = 1;
             $imageFileType = strtolower(pathinfo($fichierCible,PATHINFO_EXTENSION));
 
             // Check if image file is a actual image or fake image
-            if(isset($_POST["submit"])) {
             $check = getimagesize($_FILES["image"]["tmp_name"]);
             if($check !== false) {
                 echo "File is an image - " . $check["mime"] . ".";
@@ -46,7 +45,6 @@
             } else {
                 echo "File is not an image.";
                 $uploadOk = 0;
-            }
             }
 
             // Check file size
