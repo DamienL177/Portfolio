8766548aa41a9f7ae55d8e50ae5678b78c0bfe81
Damien Lanusse <damienl.work@gmail.com>
2023-02-06 13:07:37 +0100

    getPublication16

--- Portfolio/php/getPublication.php
+++ Portfolio/php/getPublication.php
@@ -1,6 +1,13 @@
 <?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     header('Content-Type: text/xml');
     
+
+
 
     // On définit les variables nécessaires au lien avec la BD
     $bdd = "u562708442_dlanusse";
